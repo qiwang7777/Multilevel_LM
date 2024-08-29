d2291ed58f56c87f1ce69602154f587fec36dcff
qiwang7777 <135851405+qiwang7777@users.noreply.github.com>
2024-08-29 13:58:50 +0200

    Update LM.py

--- LM.py
+++ LM.py
@@ -1,5 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 26 15:03:53 2024
 
-
+@author: wang
+"""
+import logging
 import torch
 import torch.nn as nn
 import numpy as np
@@ -203,7 +209,7 @@ class PDE:
 
 # Define the real solution functions
 def real_solution_1d(x):
-    return x**2
+    return torch.sin(x)
 
 def real_solution_2d(x, y):
     return x**2 + y**2
@@ -313,7 +319,7 @@ def Fk(input_data, model, pde, real_solution):
     
     # Compute model prediction (torch tensor)
     model_input = torch.tensor(input_data, dtype=torch.float32, requires_grad=True).reshape(-1, 1)
-    model_output = model(model_input)
+    #model_output = model(model_input)
     
     # Compute the source term from the model output (torch tensor)
     NN_source_term = pde.compute_source_term(grid_points, lambda x: model(x)).reshape(-1, 1)
@@ -322,7 +328,7 @@ def Fk(input_data, model, pde, real_solution):
     main_cost = (source_term - NN_source_term)[1:-1]
     
     return main_cost
-
+#PDE_Nontensor
 def Jk(input_data, model, pde, real_solution):
     # Compute Fk
     main_cost = Fk(input_data, model, pde, real_solution)
@@ -356,15 +362,10 @@ def Jk(input_data, model, pde, real_solution):
 # Example usage:
 # Assuming you have a PDE class and a real_solution_1d function
 
-# Define the model
-input_dim = 1
-n_hidden_layers = 1
-r_nodes_per_layer = 100
-output_dim = 1
-model = FullyConnectedNN(input_dim, n_hidden_layers, r_nodes_per_layer, output_dim)
+
 
 # Define input data
-input_data = torch.linspace(0, 1, 100)
+input_data = torch.linspace(0, 1, 40)
 
 # Define your PDE and real solution functions
 class PDE_Nontensor:
@@ -381,9 +382,9 @@ class PDE_Nontensor:
 pde = PDE_Nontensor((0,1), real_solution_1d)
 
 # Compute the Jacobian matrix
-loss_main = Fk(input_data, model, pde, real_solution_1d)
-jacobian_matrix = Jk(input_data, model, pde, real_solution_1d)
-print(0.5*(torch.randn(301).T @ jacobian_matrix.T @ jacobian_matrix @torch.randn(301)))
+#loss_main = Fk(input_data, model, pde, real_solution_1d)
+#jacobian_matrix = Jk(input_data, model, pde, real_solution_1d)
+#print(0.5*(torch.randn(301).T @ jacobian_matrix.T @ jacobian_matrix @torch.randn(301)))
     
 
 #Initialise the parameters of nn
@@ -460,7 +461,7 @@ def compute_jacobian_and_hessian(model, x):
     return jacobian, hessian
 
 # Define input tensor
-x = torch.linspace(0, 1, 40, dtype=torch.float32)
+#x = torch.linspace(0, 1, 40, dtype=torch.float32)
 
 # Compute Jacobian and Hessian
 #jaco,hess= compute_jacobian_and_hessian(My_model, x)
@@ -469,77 +470,105 @@ x = torch.linspace(0, 1, 40, dtype=torch.float32)
 
 
 
+def model_function(params,x):
+    """
+        
+
+    Parameters
+    ----------
+    params : List
+    parameters = {
+        'w1': w1,
+        'b1': b1,
+        'w2': w2,
+        'b2': b2
+       }
+        
+    """
+    x = torch.tensor(x, dtype=torch.float32).reshape(-1,1)
+    model_use =  FullyConnectedNN(input_dim,n_hidden_layers , r_nodes_per_layer, output_dim)
+    w1 = params['w1']
+    b1 = params['b1']
+    w2 = params['w2']
+    b2 = params['b2']
+    with torch.no_grad():
+        model_use.hidden_layers[0].weight = nn.Parameter(torch.tensor(w1, dtype=torch.float32))
+        model_use.hidden_layers[0].bias = nn.Parameter(torch.tensor(b1, dtype=torch.float32))
+
+        # Set weights and biases for the output layer
+        model_use.output_layer.weight = nn.Parameter(torch.tensor(w2, dtype=torch.float32))
+        model_use.output_layer.bias = nn.Parameter(torch.tensor(b2, dtype=torch.float32))
+    return model_use(x).detach().numpy()
+
 
 
 
 
    
-    
+def compute_nxx(params,x):
+    x = x.reshape(-1,1)
+    nn_out = model_function(params, x)
+    Nx = jacobian(lambda x: model_function(params,x))(x)
+    Nxx = hessian(lambda x: model_function(params,x))(x)
+    return Nxx.flatten()
+#Taylor expansion
+def taylor_loss(s,input_data, model, pde, real_solution, lambda0=0.05, regularization=False):
+    #s = torch.randn(3*r_nodes_per_layer+1)
+    input_data_tensor = torch.tensor(input_data)
+    zero_term = compute_loss(input_data, model, pde, real_solution,regularization=False)
+    F_k = Fk(input_data_tensor, model, pde, real_solution_1d)
+    J_k = Jk(input_data_tensor, model, pde, real_solution_1d)
+    first_term = F_k.T@J_k@s
+    B_k = J_k.T@J_k
+    second_term = 0.5*(s.T @ B_k @ s)
+    if regularization == True:
+        re_term = 0.5*lambda0*torch.norm(s)**2
+    else:
+        re_term = 0
+    return zero_term+first_term+second_term+re_term
+
+#Subsolver
+def line_A(input_data, model, pde, real_solution, lambda0):
+    input_data_tensor = torch.tensor(input_data)
+    #F_k = Fk(input_data_tensor, model, PDE((0,1), real_solution_1d), real_solution_1d)
+    J_k = Jk(input_data_tensor, model, pde, real_solution_1d)
+    B_k = J_k.T@J_k
+    shape_eye = B_k.shape[0]
+    return B_k+lambda0*torch.eye(shape_eye)
 
 
-    
+def line_b(input_data, model, pde, real_solution, lambda0):
+    input_data_tensor = torch.tensor(input_data)
+    F_k = Fk(input_data_tensor, model, pde, real_solution_1d)
+    J_k = Jk(input_data_tensor, model, pde, real_solution_1d)
+    return -1*J_k.T @F_k
 
 
 def LMTR(input_data, model, pde, real_solution, lambdak=0.1, regularization=False):
-    input_dim = 1
-    n_hidden_layers = 1
-    r_nodes_per_layer = 100
-    output_dim = 1
-    activation_function = torch.sigmoid
-    def model_function(params,x):
-        """
-        
-
-        Parameters
-        ----------
-        params : List
-            parameters = {
-            'w1': w1,
-            'b1': b1,
-            'w2': w2,
-            'b2': b2
-        }
-        
-        """
-        x = torch.tensor(x, dtype=torch.float32).reshape(-1,1)
-        model_use =  FullyConnectedNN(input_dim,n_hidden_layers , r_nodes_per_layer, output_dim)
-        w1 = params['w1']
-        b1 = params['b1']
-        w2 = params['w2']
-        b2 = params['b2']
-        with torch.no_grad():
-            model_use.hidden_layers[0].weight = nn.Parameter(torch.tensor(w1, dtype=torch.float32))
-            model_use.hidden_layers[0].bias = nn.Parameter(torch.tensor(b1, dtype=torch.float32))
-
-            # Set weights and biases for the output layer
-            model_use.output_layer.weight = nn.Parameter(torch.tensor(w2, dtype=torch.float32))
-            model_use.output_layer.bias = nn.Parameter(torch.tensor(b2, dtype=torch.float32))
-        return model_use(x).detach().numpy()
-    def compute_nxx(params,x):
-        x = x.reshape(-1,1)
-        nn_out = model_function(params, x)
-        Nx = jacobian(lambda x: model_function(params,x))(x)
-        Nxx = hessian(lambda x: model_function(params,x))(x)
-        return Nxx.flatten()
-    
-    Model = FullyConnectedNN(input_dim, n_hidden_layers, r_nodes_per_layer, output_dim, activation_function)
+    #input_data = torch.tensor(input_data)
+    input_dim = model.hidden_layers[0].in_features
+    #n_hidden_layers = 1
+    r_nodes_per_layer = model.hidden_layers[0].out_features
+    #output_dim = 1
+    #activation_function = torch.sigmoid
+    #Set up the neural network
+    #Model = FullyConnectedNN(input_dim, n_hidden_layers, r_nodes_per_layer, output_dim, activation_function)
     #Initialize the neural network
-    model_param_init = initialize_parameters(input_dim, r_nodes_per_layer)
-    w10 = model_param_init['w1']
-    b10 = model_param_init['b1']
-    w20 = model_param_init['w2']
-    b20 = model_param_init['b2']
-    with torch.no_grad():
-        # Set weights and biases for the first hidden layer
-        Model.hidden_layers[0].weight = nn.Parameter(torch.tensor(w10, dtype=torch.float32))
-        Model.hidden_layers[0].bias = nn.Parameter(torch.tensor(b10, dtype=torch.float32))
-
-        # Set weights and biases for the output layer
-        Model.output_layer.weight = nn.Parameter(torch.tensor(w20, dtype=torch.float32))
-        Model.output_layer.bias = nn.Parameter(torch.tensor(b20, dtype=torch.float32))
+    #model_param_init = initialize_parameters(input_dim, r_nodes_per_layer)
+    w10 = model.hidden_layers[0].weight.data.numpy()
+    b10 = model.hidden_layers[0].bias.data.numpy()
+    w20 = model.output_layer.weight.data.numpy()
+    b20 = model.output_layer.bias.data.numpy()
+    #model_param_init = initialize_parameters(input_dim, r_nodes_per_layer)
+    #w10 = model_param_init['w1']
+    #b10 = model_param_init['b1']
+    #w20 = model_param_init['w2']
+    #b20 = model_param_init['b2']
     
-    model_NN = model_nn(input_data,model = Model)
-    Loss = loss_solve_pde(model_NN, np.linspace(0,1,40),regularization=True,lambdak=0.1)
+    
+    #model_NN = model_nn(input_data,model = Model)
+    
+    #Loss = compute_loss(input_data, model, pde, real_solution,lambdak,True)
     ###Optional parameters
     eta1 = 0.1
     eta2 = 0.75
@@ -549,95 +578,129 @@ def LMTR(input_data, model, pde, real_solution, lambdak=0.1, regularization=Fals
     lambda0 = 0.05
     lambda_min = 10**(-4)
     epsilon = 10**(-4)
-    max_iter = 10000
+    max_iter = 1000
     k = 0
     #Get the gradient
     
-    input_data_tensor = torch.tensor(input_data, dtype=torch.float32)
-    model_tt = FullyConnectedNN(input_dim, n_hidden_layers, r_nodes_per_layer, output_dim)
-    loss = compute_loss(input_data_tensor, model_tt, PDE((0,1), real_solution_1d), real_solution_1d, lambdak=0.1, regularization=True)
+    input_data_tensor = torch.tensor(input_data)
+    #model_tt = FullyConnectedNN(input_dim, n_hidden_layers, r_nodes_per_layer, output_dim)
+    loss = compute_loss(input_data_tensor, model, pde, real_solution, lambdak, regularization=True)
 
     # Compute gradients
-    model_tt.zero_grad()
+    model.zero_grad()
     loss.backward()  # This should now work since loss is a PyTorch tensor
 
     # Access gradients
-    gradients = torch.cat([param.grad.view(-1) for param in model_tt.parameters()])
-    w10_1d = w10.squeeze()
-    w20_1d = w20.squeeze()
+    gradients = torch.cat([param.grad.view(-1) for param in model.parameters()])
+    w10_1d = torch.tensor(w10.squeeze())
+    w20_1d = torch.tensor(w20.squeeze())
+    b10 = torch.tensor(b10)
+    b20 = torch.tensor(b20)
     allparams = torch.cat([w10_1d,b10,w20_1d,b20])
-    while k<=max_iter and np.linalg.norm(gradients)<=epsilon:
-        def taylor_loss(s,input_data, model, pde, real_solution, lambdak=0.1, regularization=False):
-            #s = torch.randn(3*r_nodes_per_layer+1)
-            zero_term = compute_loss(input_data, model, pde, real_solution,regularization=False)
-            F_k = Fk(input_data_tensor, model, PDE((0,1), real_solution_1d), real_solution_1d)
-            J_k = Jk(input_data_tensor, model, PDE((0,1), real_solution_1d), real_solution_1d)
-            first_term = F_k.T@J_k@s
-            B_k = J_k.T@J_k
-            second_term = 0.5*(s.T@B_k@s)
-            if regularization == True:
-                re_term = 0.5*lambda0*torch.norm(s)**2
-            else:
-                re_term = 0
-            return zero_term+first_term+second_term+re_term
+    while k<=max_iter and np.linalg.norm(gradients)>=epsilon:
+        #print(np.linalg.norm(gradients))
+        A_sub = line_A(input_data, model, pde, real_solution, lambda0)
+        b_sub = line_b(input_data, model, pde, real_solution, lambda0)
+        A_np = A_sub.detach().numpy()
+        b_np = b_sub.detach().numpy()
+
+        s_np, info = cg(A_np,b_np)
+        s = torch.from_numpy(s_np)
+        #print(s)
         
-        def line_A(input_data, model, pde, real_solution, lambdak=0.1, regularization=False):
-            F_k = Fk(input_data_tensor, model, PDE((0,1), real_solution_1d), real_solution_1d)
-            J_k = Jk(input_data_tensor, model, PDE((0,1), real_solution_1d), real_solution_1d)
-            B_k = J_k.T@J_k
-            return B_k
-        def line_b(input_data, model, pde, real_solution, lambdak=0.1, regularization=False):
-            F_k = Fk(input_data_tensor, model, PDE((0,1), real_solution_1d), real_solution_1d)
-            J_k = Jk(input_data_tensor, model, PDE((0,1), real_solution_1d), real_solution_1d)
-            return -1*J_k.T @F_k
-        A_sub = line_A(input_data, model, pde, real_solution, lambdak=0.1, regularization=False)
-        b_sub = line_b(input_data, model, pde, real_solution, lambdak=0.1, regularization=False)
-        s = torch.zeros_like(b_sub)
-        s = cg(A_sub,b_sub,s = s)
-        pred = taylor_loss(torch.zeros_like(b_sub), input_data, model, pde, real_solution)-taylor_loss(s, input_data, model, pde, real_solution)
-        fk = compute_loss(input_data, Model, pde, real_solution, lambdak=0.1, regularization=True)
+        pred = taylor_loss(torch.zeros_like(b_sub), input_data, model, pde, real_solution,lambda0,regularization=True)-taylor_loss(s, input_data, model, pde, real_solution,lambda0,regularization=True)
+        fk = compute_loss(input_data, model, pde, real_solution, lambdak, regularization=True)
         new_params = allparams+s
         w10_new = new_params[:r_nodes_per_layer].view((r_nodes_per_layer,1))
         b10_new = new_params[r_nodes_per_layer:2*r_nodes_per_layer]
-        w20_new = new_params[2*r_nodes_per_layer:3*r_nodes_per_layer].vew((1,r_nodes_per_layer))
+        w20_new = new_params[2*r_nodes_per_layer:3*r_nodes_per_layer].view((1,r_nodes_per_layer))
         b20_new = new_params[-1]
+        model_new = model
         with torch.no_grad():
             # Set weights and biases for the first hidden layer
-            Model.hidden_layers[0].weight = nn.Parameter(torch.tensor(w10_new, dtype=torch.float32))
-            Model.hidden_layers[0].bias = nn.Parameter(torch.tensor(b10_new, dtype=torch.float32))
+            model_new.hidden_layers[0].weight = nn.Parameter(torch.tensor(w10_new, dtype=torch.float32))
+            model_new.hidden_layers[0].bias = nn.Parameter(torch.tensor(b10_new, dtype=torch.float32))
 
             # Set weights and biases for the output layer
-            Model.output_layer.weight = nn.Parameter(torch.tensor(w20_new, dtype=torch.float32))
-            Model.output_layer.bias = nn.Parameter(torch.tensor(b20_new, dtype=torch.float32))
+            model_new.output_layer.weight = nn.Parameter(torch.tensor(w20_new, dtype=torch.float32))
+            model_new.output_layer.bias = nn.Parameter(torch.tensor(b20_new, dtype=torch.float32))
         
-        model_NN = model_nn(input_data,model = Model)
-        fks = compute_loss(input_data, Model, pde, real_solution, lambdak=0.1, regularization=True)
+        #model_NN = model_nn(input_data,model = model)
+        fks = compute_loss(input_data, model_new, pde, real_solution, lambdak, regularization=True)
         ared = fk-fks
+        #print(pred)
         pho = ared/pred
-        if pho >= eta1:
+        print(pho)
+        if pred == 0:
+            break
+        elif pho >= eta1:
             w10 = w10_new
             b10 = b10_new
             w20 = w20_new
             b20 = b20_new
+            #model = model_new
             if pho>=eta2:
-                lambdak = np.max(lambda_min,gamma2*lambdak)
+                lambda0 = max(lambda_min,gamma2*lambda0)
             else:
-                lambdak = np.max(lambda_min,gamma1*lambdak)
+                lambda0 = max(lambda_min,gamma1*lambda0)
         else:
             w10 = w10
             b10 = b10
             w20 = w20
             b20 = b20
-            lambdak = gamma3*lambdak
+            #model = model
+            lambda0 = gamma3*lambda0
         
-        k += 1
+        
         with torch.no_grad():
             # Set weights and biases for the first hidden layer
-            Model.hidden_layers[0].weight = nn.Parameter(torch.tensor(w10, dtype=torch.float32))
-            Model.hidden_layers[0].bias = nn.Parameter(torch.tensor(b10, dtype=torch.float32))
+            model_new.hidden_layers[0].weight = nn.Parameter(torch.tensor(w10, dtype=torch.float32))
+            model_new.hidden_layers[0].bias = nn.Parameter(torch.tensor(b10, dtype=torch.float32))
 
             # Set weights and biases for the output layer
-            Model.output_layer.weight = nn.Parameter(torch.tensor(w20, dtype=torch.float32))
-            Model.output_layer.bias = nn.Parameter(torch.tensor(b20, dtype=torch.float32))
+            model_new.output_layer.weight = nn.Parameter(torch.tensor(w20, dtype=torch.float32))
+            model_new.output_layer.bias = nn.Parameter(torch.tensor(b20, dtype=torch.float32))
+        model = model_new
+        loss = compute_loss(input_data_tensor, model, pde, real_solution, lambdak, regularization=True)
+
+        # Compute gradients
+        model.zero_grad()
+        loss.backward()  # This should now work since loss is a PyTorch tensor
+
+        # Access gradients
+        gradients = torch.cat([param.grad.view(-1) for param in model.parameters()])
+    
+        k += 1
+        
+    input_data = torch.tensor(input_data.reshape(sample_num,input_dim), dtype=torch.float32)
             
-        return Model
+    return model(input_data)
+            
+        
+#Test for LMTR
+input_dim = 1
+n_hidden_layers = 1
+r_nodes_per_layer = 100
+output_dim = 1
+activation_function = torch.tanh
+model = FullyConnectedNN(input_dim, n_hidden_layers, r_nodes_per_layer, output_dim, activation_function)
+initial_params = initialize_parameters(input_dim,r_nodes_per_layer)
+init_w1 = initial_params['w1']
+init_b1 = initial_params['b1']
+init_w2 = initial_params['w2']
+init_b2 = initial_params['b2']
+with torch.no_grad():
+    model.hidden_layers[0].weight = nn.Parameter(torch.tensor(init_w1, dtype=torch.float32))
+    model.hidden_layers[0].bias = nn.Parameter(torch.tensor(init_b1, dtype=torch.float32))
+
+        # Set weights and biases for the output layer
+    model.output_layer.weight = nn.Parameter(torch.tensor(init_w2, dtype=torch.float32))
+    model.output_layer.bias = nn.Parameter(torch.tensor(init_b2, dtype=torch.float32))
+
+#print(model(torch.tensor(input_data.reshape(40,1), dtype=torch.float32)))
+
+#print(real_solution_1d(input_data))
+print(LMTR(input_data, model, pde, real_solution_1d, lambdak=0.1, regularization=True))
+
+#print(compute_loss(input_data, model, pde, real_solution_1d)) 
+#print(loss_solve_pde(model, input_data))
