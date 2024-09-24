dd10df9d9114dfcb2e27ce667722cdd8b2f97e5b
qiwang7777 <135851405+qiwang7777@users.noreply.github.com>
2024-09-24 14:52:11 +0200

    Update and rename test.py to params_options.py

--- main_lm/params_options.py
+++ main_lm/params_options.py
@@ -31,32 +31,7 @@ class LMTR_params_options:
         assert epsilon>0 
         
 
-from Multilevel_LM.main_lm.LMTR_poisson import LMTR_solving_poisson
-import numpy as np
-import matplotlib.pyplot as plt
 
-def test_func_1d(x):
-    return torch.sin(x)
-from Multilevel_LM.main_lm.neural_network_construction import FullyConnectedNN
-input_dim = 1
-output_dim = 1
-n_hidden_layers = 1
-r_nodes_per_layer = 300
-model_21 = FullyConnectedNN(input_dim, n_hidden_layers, r_nodes_per_layer, output_dim)
-sample_num = 41
-x_1d = torch.tensor(np.linspace(0,1,sample_num).reshape(sample_num,1), dtype=torch.float32)
-pred_1d = LMTR_solving_poisson(test_func_1d, model_21, x_1d, lambdak=0.1).detach().numpy()
-real_1d = test_func_1d(x_1d).detach().numpy()
-plt.figure(figsize = (10,5))
-x_1d_np = x_1d.detach().numpy()
-plt.plot(x_1d_np,pred_1d,label = 'Model Output',color = 'blue',linestyle='-',marker = 'o',markersize=2)
-plt.plot(x_1d_np, real_1d, label='Real Solution', color='red', linestyle='dashed')
-plt.title('Plot of Model Output vs. Input x')
-plt.xlabel('Input x')
-plt.ylabel('Model Output')
-plt.legend()
-plt.grid()
-plt.show()
 
 
 
