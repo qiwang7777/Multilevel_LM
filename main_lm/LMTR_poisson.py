c23cd8ecac8572aa16b56f1ffea96aead520cb67
qiwang7777 <135851405+qiwang7777@users.noreply.github.com>
2024-09-24 14:09:49 +0200

    Update LMTR_poisson.py

--- main_lm/LMTR_poisson.py
+++ main_lm/LMTR_poisson.py
@@ -10,8 +10,9 @@ from Multilevel_LM.main_lm.loss_poisson import loss_solving_poisson,compute_loss
 from Multilevel_LM.main_lm.subsolver_poisson import Taylor_solver,sub_A_solving_poisson,sub_b_solving_poisson
 import torch
 import copy
-from scipy.sparse.linalg import cg
+from scipy.sparse.linalg import cg, LinearOperator,splu
 import numpy as np
+from scipy.sparse import csc_matrix
 
 
 
@@ -64,6 +65,7 @@ def update_model_parameters(model, s):
         
 def LMTR_solving_poisson(real_solution,model,x,lambdak,regularization=True,lambdap=0.1):
     fk = loss_solving_poisson(real_solution, model, x)
+    
     options = LMTR_params_options()
     eta1 = options.eta1
     eta2 = options.eta2
@@ -72,34 +74,87 @@ def LMTR_solving_poisson(real_solution,model,x,lambdak,regularization=True,lambd
     gamma3 = options.gamma3
     lambda_min = options.lambda_min
     epsilon = options.epsilon
-    max_iter = options.max_iter
+    max_iter = 100
     k=0
     while torch.norm(compute_loss_gradients(real_solution,model,x,regularization=True,lambdap = 0.1))>=epsilon and k<=max_iter:
+        print(torch.norm(compute_loss_gradients(real_solution,model,x,regularization=True,lambdap = 0.1))) 
+        
         A = sub_A_solving_poisson(real_solution,model,x,lambdak)
+        #check the reason why CG didn't converge
+        #check if A is poor conditioning
+        #AA = A @ A.T
+        #singular_values = torch.svd(AA).S
+        #cond_number = singular_values[0] / singular_values[-1]  # max/smallest singular value
+
+        # Define a threshold for poor conditioning
+        #threshold = 1e10  # Example threshold
+        #if cond_number >threshold:
+        #    print(f"Matrix A is poorly conditioned")
+          
+        #check if A is SPD
+        #def is_spd(A):
+        #    if not torch.allclose(A,A.T):
+        #        print("Matrix A is not symetric")
+        #        return 
+        #    eigenvalues = torch.linalg.eigvals(A).real
+        #    if torch.all(eigenvalues <= 0):
+        #        print("Matrix A is not positive definite.")
+        #        return
+            
+        #is_spd(A)
+        
+       
+        
+
+
         b = (-1)*sub_b_solving_poisson(real_solution,model,x,lambdak)
-        s,info = cg(A.detach().numpy(),b.detach().numpy().flatten())
+        #A_sparse = csc_matrix(A.detach().numpy())
+        
+        s = np.linalg.solve(A.detach().numpy(),b.detach().numpy().flatten())
+        #try:
+        #    s,info = cg(A.detach().numpy(),b.detach().numpy().flatten())
+        #    if info > 0:
+        #        print(f"Conjugate gradient did not converge after {info} iterations.")
+        #        break
+        #except Exception as e:
+        #    print(f"Error in conjugate gradient solver: {e}")
+        #    break
+    
+        
         s = torch.tensor(s)
         new_model = update_model_parameters(model, s)[1]
         fks = loss_solving_poisson(real_solution, new_model, x)
-        mks = Taylor_solver(real_solution,new_model,x,lambdak,s)
+        mks = Taylor_solver(real_solution,model,x,lambdak,s)
         fk = loss_solving_poisson(real_solution, model, x)
+        #print(fk)
         mk = Taylor_solver(real_solution, model, x, lambdak, torch.zeros(sum(p.numel() for p in model.parameters())))
+        #print(mk)
         ared = fk-fks
         pred = mk-mks
+        
         pho = ared/pred
-        #print(pred)
-        if pho >= eta1:
-            model = new_model
-            if pho >= eta2:
-                lambdak = max(lambda_min,gamma2*lambdak)
-            else:
-                lambdak = max(lambda_min,gamma1*lambdak)
+        #print(pho)
+        print(fk)
+        
+        if pred == 0 :
+            print("pred = 0")
+        
         else:
-            model = model
-            lambdak = gamma3*lambdak
+       
+    
+            if pho >= eta1:
+                model = new_model
+                if pho >= eta2:
+                    lambdak = max(lambda_min,gamma2*lambdak)
+                else:
+                    lambdak = max(lambda_min,gamma1*lambdak)
+            else:
+                model = model
+                lambdak = gamma3*lambdak
             
         k+=1
     return model(x)
+    
     
 #Test
 #def test_func_1d(x):
