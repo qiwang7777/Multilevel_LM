019c7b6e05af396574fc7f7343f69127e8bc9768
qiwang7777 <135851405+qiwang7777@users.noreply.github.com>
2025-08-28 17:38:33 +0200

    Create setDefaultParameters_new.py

---
+++ non_smooth/setDefaultParameters_new.py
@@ -0,0 +1,62 @@
+import numpy as np
+def set_default_parameters(name):
+    params = {}
+
+    # General Parameters
+    params['spsolver']  = name.replace(' ', '')
+    params['outFreq']   = 1
+    params['debug']     = False
+    params['initProx']  = False
+    params['t']         = 1
+    params['safeguard'] = np.sqrt(np.finfo(float).eps)
+
+    # Stopping tolerances
+    params['maxit']   = 200
+    params['reltol']  = False
+    params['gtol']    = 1e-5
+    params['stol']    = 1e-10
+    params['ocScale'] = params['t']
+
+    # Trust-region parameters
+    params['eta1']     = 0.05
+    params['eta2']     = 0.9
+    params['gamma1']   = 0.25
+    params['gamma2']   = 2.5
+    params['delta']    = 1.e2
+    params['deltamax'] = 1e10
+
+    # Subproblem solve tolerances
+    params['atol']    = 1e-5
+    params['rtol']    = 1e-3
+    params['spexp']   = 2
+    params['maxitsp'] = 15
+
+    # GCP and subproblem solve parameter
+    params['useGCP']    = False
+    params['mu1']       = 1e-4
+    params['beta_dec']  = 0.1
+    params['beta_inc']  = 10.0
+    params['maxit_inc'] = 2
+
+    # SPG and spectral GCP parameters
+    params['lam_min'] = 1e-12
+    params['lam_max'] = 1e12
+
+    # Inexactness parameters
+    params['useInexactObj']  = False
+    params['useInexactGrad'] = False
+    params['gradTol']        = np.sqrt(np.finfo(float).eps)
+
+    # Recursive Parameters
+    params['RgnormScale']      = 1e-1 # is v in Rgnorm >= v*gnorm -> relative R-step flag
+    params['RgnormScaleTol']   = 5e2  # is v in Rgnorm >= v^i*gtol -> absolute R-step flag
+    
+    params['lin_corr_theta'] = 0.8
+    params['coarse_pos_floor'] = 0.0
+    params['coarse_pos_floor_rel'] = 0.25
+    params['coarse_delta_min_frac'] = 0.05
+    
+
+
+
+    return params
